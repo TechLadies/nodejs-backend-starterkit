7066711d1b847369528c313af3c098b1071e8fa5
Michael Cheng <mcheng.work@gmail.com>
2019-07-28 17:24:33 +0800

    Run jsfmt on the files

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res, next) {
     attributes: ['firstName', 'lastName', 'email']
   }).then(users => {
     res.json(users)
-  });
+  })
 })
 
 module.exports = router
