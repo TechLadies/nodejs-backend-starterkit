6137ce4f01f22fd35ce580739f494224baf29748
Michael Cheng <mcheng.work@gmail.com>
2019-07-28 11:52:08 +0800

    Enhance developer experience
    - Add Nodemon to see immediate results when changing files
    - Implemented debug on dev

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const debug = require('debug')('app:users')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+  debug('Hello World!')
+  res.send('respond with a resource now!')
 })
 
 module.exports = router
