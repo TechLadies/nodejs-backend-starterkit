5d293d4a82c90fea20984adc57d21af08d6a4659
Michael Cheng <mcheng.work@gmail.com>
2019-07-28 17:19:36 +0800

    Change to querying records in the database.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const debug = require('debug')('app:users')
+const db = require('../models/index')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   debug('Hello World!')
 
-  const users = [
-    { name: 'Elisha', gender: 'FEMALE' },
-    { name: 'Michael', gender: 'MALE' }
-  ]
-
-  res.json(users)
+  db.User.findAll({
+    attributes: ['firstName', 'lastName', 'email']
+  }).then(users => {
+    res.json(users)
+  });
 })
 
 module.exports = router
