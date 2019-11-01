9b62f75fe591e049f83ade2a0e5b900338f410b2
Michael Cheng <mcheng.work@gmail.com>
2019-11-02 00:51:21 +0800

    Update app with latest libraries.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const session = require('express-session')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -17,6 +18,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+)
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
