91c02768865c99fc63d50ae324fa4b81cf5cd0d8
Thomas Chia <thomas.h.chia@googlemail.com>
2020-06-28 17:07:40 +0800

    Updated README and added knex test config.

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,16 @@ module.exports = {
     connection: {
       user: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD,
-      database: 'starterkit_development'
+      database: 'starterkit_development',
+    },
+  },
+  test: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      user: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: 'starterkit_test',
     },
   },
 }
